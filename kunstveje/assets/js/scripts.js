c9b3bb32850890aaba500a3fd4fbba842c06e058
joachimbl <joachim.blicher@gmail.com>
2017-05-09 12:04:27 +0200

    kunstveje

---
+++ kunstveje/assets/js/scripts.js
@@ -0,0 +1,29 @@
+// Go to frontpage after inaktivity
+
+var IDLE_TIMEOUT = 900; //seconds
+var _idleSecondsCounter = 0;
+document.onclick = function() {
+    _idleSecondsCounter = 0;
+};
+document.onmousemove = function() {
+    _idleSecondsCounter = 0;
+};
+document.onscroll = function() {
+    _idleSecondsCounter = 0;
+};
+document.onkeypress = function() {
+    _idleSecondsCounter = 0;
+};
+window.setInterval(CheckIdleTime, 1000);
+
+function CheckIdleTime() {
+    _idleSecondsCounter++;
+    var oPanel = document.getElementById("SecondsUntilExpire");
+    if (oPanel)
+        oPanel.innerHTML = (IDLE_TIMEOUT - _idleSecondsCounter) + "";
+    if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+        document.location.href = "index.html";
+    }
+};
+
+document.addEventListener("touchstart", function(){}, true);
